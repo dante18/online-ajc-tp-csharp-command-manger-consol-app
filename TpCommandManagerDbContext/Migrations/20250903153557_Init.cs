9ed055a50d37170c8c04bb41b4b26c45ef41f5b7
dante18 <lablandinerie2@gmail.com>
2025-09-03 17:51:44 +0200

    Add migration files

---
+++ TpCommandManagerDbContext/Migrations/20250903153557_Init.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TpCommandManagerDbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adresse",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Rue = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ville = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodePostal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pays = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adresse", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telephone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pate", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prix = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produit", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commande",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateCommande = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateLivraison = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    AdresseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commande", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commande_Adresse_AdresseId",
+                        column: x => x.AdresseId,
+                        principalTable: "Adresse",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Commande_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Boisson",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Petillant = table.Column<bool>(type: "bit", nullable: false),
+                    Kcal = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boisson", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Boisson_Produit_Id",
+                        column: x => x.Id,
+                        principalTable: "Produit",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nourriture",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Vegetarien = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nourriture", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Nourriture_Produit_Id",
+                        column: x => x.Id,
+                        principalTable: "Produit",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProduitCommande",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommandeId = table.Column<int>(type: "int", nullable: false),
+                    ProduitId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProduitCommande", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProduitCommande_Commande_CommandeId",
+                        column: x => x.CommandeId,
+                        principalTable: "Commande",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Burger",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Burger", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Burger_Nourriture_Id",
+                        column: x => x.Id,
+                        principalTable: "Nourriture",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pasta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Kcal = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pasta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pasta_Nourriture_Id",
+                        column: x => x.Id,
+                        principalTable: "Nourriture",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pizza",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    PateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizza", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pizza_Nourriture_Id",
+                        column: x => x.Id,
+                        principalTable: "Nourriture",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pizza_Pate_PateId",
+                        column: x => x.PateId,
+                        principalTable: "Pate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kcal = table.Column<float>(type: "real", nullable: false),
+                    EstAllergene = table.Column<bool>(type: "bit", nullable: false),
+                    EstVegetarien = table.Column<bool>(type: "bit", nullable: false),
+                    BurgerId = table.Column<int>(type: "int", nullable: true),
+                    PizzaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredient", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredient_Burger_BurgerId",
+                        column: x => x.BurgerId,
+                        principalTable: "Burger",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Ingredient_Pizza_PizzaId",
+                        column: x => x.PizzaId,
+                        principalTable: "Pizza",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commande_AdresseId",
+                table: "Commande",
+                column: "AdresseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commande_ClientId",
+                table: "Commande",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredient_BurgerId",
+                table: "Ingredient",
+                column: "BurgerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredient_PizzaId",
+                table: "Ingredient",
+                column: "PizzaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pizza_PateId",
+                table: "Pizza",
+                column: "PateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProduitCommande_CommandeId",
+                table: "ProduitCommande",
+                column: "CommandeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Boisson");
+
+            migrationBuilder.DropTable(
+                name: "Ingredient");
+
+            migrationBuilder.DropTable(
+                name: "Pasta");
+
+            migrationBuilder.DropTable(
+                name: "ProduitCommande");
+
+            migrationBuilder.DropTable(
+                name: "Burger");
+
+            migrationBuilder.DropTable(
+                name: "Pizza");
+
+            migrationBuilder.DropTable(
+                name: "Commande");
+
+            migrationBuilder.DropTable(
+                name: "Nourriture");
+
+            migrationBuilder.DropTable(
+                name: "Pate");
+
+            migrationBuilder.DropTable(
+                name: "Adresse");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Produit");
+        }
+    }
+}
