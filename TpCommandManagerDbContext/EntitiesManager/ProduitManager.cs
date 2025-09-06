061af4cec3d5f11d8e7a2c8bdc85b8c43cfe63da
dante18 <lablandinerie2@gmail.com>
2025-09-06 19:26:38 +0200

    Add class used to manage crud action for command

--- TpCommandManagerDbContext/EntitiesManager/ProduitManager.cs
+++ TpCommandManagerDbContext/EntitiesManager/ProduitManager.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using TpCommandManagerDbContext.Entities;
+
+namespace TpCommandManagerDbContext.EntitiesManager;
+
+public class ProduitManager
+{
+    private readonly TpCommandManagerContext _context;
+
+    public ProduitManager(TpCommandManagerContext context)
+    {
+        _context = context;
+    }
+
+    public List<Produit> ObtenirListProduit()
+    {
+        return this._context.Produit.Include(p => p.ProduitCommande).ToList();
+    }
+
+    public Produit ObtenirProduit(int id)
+    {
+        return this._context.Produit.Include(p => p.ProduitCommande).Where(Produit => Produit.Id == id).FirstOrDefault();
+    }
+}
