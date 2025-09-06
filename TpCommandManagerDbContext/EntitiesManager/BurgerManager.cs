0ec7ec13591433e211e91bed3d18de6a8198e75b
dante18 <lablandinerie2@gmail.com>
2025-09-06 19:27:36 +0200

    Add class used to manage crud action for burger

--- TpCommandManagerDbContext/EntitiesManager/BurgerManager.cs
+++ TpCommandManagerDbContext/EntitiesManager/BurgerManager.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using TpCommandManagerDbContext.Entities;
+
+namespace TpCommandManagerDbContext.EntitiesManager;
+
+public class BurgerManager
+{
+    private readonly TpCommandManagerContext _context;
+
+    public BurgerManager(TpCommandManagerContext context)
+    {
+        _context = context;
+    }
+
+    public List<Burger> ObtenirListBurger()
+    {
+        return this._context.Burger.Include(p => p.Ingredients).ToList();
+    }
+
+    public Burger ObtenirBurger(int id)
+    {
+        return this._context.Burger.Include(p => p.Ingredients).Where(burger => burger.Id == id).FirstOrDefault();
+    }
+
+    public void AjouterBurger(Burger burger)
+    {
+        this._context.Burger.Add(burger);
+        this._context.SaveChanges();
+    }
+
+    public void MiseAJourBurger(Burger burger)
+    {
+        this._context.Burger.Update(burger);
+        this._context.SaveChanges();
+    }
+
+    public void SupprimerBurger(Burger burger)
+    {
+        this._context.Burger.Remove(burger);
+        this._context.SaveChanges();
+    }
+}
